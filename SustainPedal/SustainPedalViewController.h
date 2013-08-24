0dd95997e6744ccd3ffade8e47ac97bfd157b93c
Keijiro Takahashi <keijiro@MSX.local>
2013-08-24 18:33:16 +0900

    First implementation.

--- SustainPedal/SustainPedalViewController.h
+++ SustainPedal/SustainPedalViewController.h
@@ -1,13 +1,11 @@
-//
-//  SustainPedalViewController.h
-//  SustainPedal
-//
-//  Created by Keijiro Takahashi on 8/24/13.
-//  Copyright (c) 2013 Keijiro Takahashi. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
+#import <CoreMIDI/CoreMIDI.h>
 
 @interface SustainPedalViewController : UIViewController
+{
+    MIDIClientRef client;
+    MIDIPortRef outputPort;
+    int touchCount;
+}
 
 @end
--- SustainPedal/SustainPedalViewController.m
+++ SustainPedal/SustainPedalViewController.m
@@ -1,14 +1,8 @@
-//
-//  SustainPedalViewController.m
-//  SustainPedal
-//
-//  Created by Keijiro Takahashi on 8/24/13.
-//  Copyright (c) 2013 Keijiro Takahashi. All rights reserved.
-//
-
 #import "SustainPedalViewController.h"
 
 @interface SustainPedalViewController ()
+
+- (void)sendSustainPedalMessage:(int)param;
 
 @end
 
@@ -17,13 +11,61 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    self.view.backgroundColor = UIColor.grayColor;
+
+    MIDIClientCreate(CFSTR("SustainPedal Client"), nil, nil, &client);
+    MIDIOutputPortCreate(client, CFSTR("SustainPedal Output Port"), &outputPort);
 }
 
-- (void)didReceiveMemoryWarning
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    if (self->touchCount == 0) {
+        [self sendSustainPedalMessage:0x7f];
+        self.view.backgroundColor = UIColor.whiteColor;
+    }
+    
+    self->touchCount += touches.count;
+    
+    [super touchesBegan:touches withEvent:event];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    self->touchCount -= touches.count;
+
+    if (self->touchCount == 0) {
+        [self sendSustainPedalMessage:0];
+        self.view.backgroundColor = UIColor.grayColor;
+    }
+    
+    [super touchesEnded:touches withEvent:event];
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    self->touchCount -= touches.count;
+    
+    if (self->touchCount == 0) {
+        [self sendSustainPedalMessage:0];
+        self.view.backgroundColor = UIColor.grayColor;
+    }
+
+    [super touchesCancelled:touches withEvent:event];
+}
+
+- (void)sendSustainPedalMessage:(int)param
+{
+    const Byte message[] = {0xb0, 0x40, param};
+    
+    MIDIPacketList packetList;
+    MIDIPacket *packet = MIDIPacketListInit(&packetList);
+    MIDIPacketListAdd(&packetList, sizeof(packetList), packet, 0, sizeof(message), message);
+    
+    ItemCount destinationCount = MIDIGetNumberOfDestinations();
+    for (int i = 0; i < destinationCount; i++) {
+        MIDISend(outputPort, MIDIGetDestination(i), &packetList);
+    }
 }
 
 @end
